3c9e3ca8f4bdd07410774ee3a95a950d9f5f886f
MagicBag-tab <est24347@uvg.edu.gt>
2024-11-13 20:31:55 -0600

    Modificaciones

--- Sistema.java
+++ Sistema.java
@@ -1,17 +1,25 @@
-public class Sistema{
-    public interface Sistema {
-        void encender();
-        void apagar();
-        void ajustarTemperatura(int incremento);
-        void activarModoAutomatico();
-        void configurarDistribucionAire(String distribucion);
-        void activarDesempañador();
-        void desactivarDesempañador();
-        boolean isEstado();
-        int getTemperatura();
-        boolean isModoAutomatico();
-        String getDistribucionAire();
-        boolean isDesempañadorActivo();
-    }
-    
+public interface Sistema {
+    void encender();
+
+    void apagar();
+
+    void ajustarTemperatura(int incremento);
+
+    void activarModoAutomatico();
+
+    void configurarDistribucionAire(String distribucion);
+
+    void activarDesempañador();
+
+    void desactivarDesempañador();
+
+    boolean isEstado();
+
+    int getTemperatura();
+
+    boolean isModoAutomatico();
+
+    String getDistribucionAire();
+
+    boolean isDesempañadorActivo();
 }
