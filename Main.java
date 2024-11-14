ea42a038ea5d3fd661df83017dbd87372eaa2142
MagicBag-tab <est24347@uvg.edu.gt>
2024-11-13 20:59:06 -0600

    Case 1 y 2 ya hechos, se edito para que no haya un blucle infinito

--- Main.java
+++ Main.java
@@ -29,11 +29,13 @@ public class Main {
 
             switch (op) {
                 case 1:
-
+                    clase.encender();
+                    System.out.println("Sistema encendido");
                     break;
 
                 case 2:
-
+                    clase.apagar();
+                    System.out.println("Sistema apagado");
                     break;
 
                 case 3:
@@ -83,6 +85,25 @@ public class Main {
                 default:
                     break;
             }
+
+            System.out.println("------MENU-----");
+
+            System.out.println("1. Encender sistema");
+            System.out.println("2. Apagar sistema");
+            System.out.println("3. Ajustar temperatura");
+            System.out.println("4. Activar modo automático");
+            System.out.println("5. Configurar distribución de aire");
+            System.out.println("6. Activar desempañador");
+            System.out.println("7. Desactivar desempañador");
+            System.out.println("8. Ajustar nivel de ventilación");
+            System.out.println("9. Configurar zona de ventilación");
+            System.out.println("10. Activar modo silencioso");
+            System.out.println("11. Ajustar nivel de humedad");
+            System.out.println("12. Ver estado del sistema");
+            System.out.println("13. Salir");
+
+            op = scanner.nextInt();
+            scanner.nextLine();
 
         }
 
