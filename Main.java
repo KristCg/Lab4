e9d228c304b07a57e0783da33becd06dd3c17c6c
MagicBag-tab <est24347@uvg.edu.gt>
2024-11-13 20:51:28 -0600

    Todos los case, se empieza a editarlos

--- Main.java
+++ Main.java
@@ -32,6 +32,54 @@ public class Main {
 
                     break;
 
+                case 2:
+
+                    break;
+
+                case 3:
+
+                    break;
+
+                case 4:
+
+                    break;
+
+                case 5:
+
+                    break;
+
+                case 6:
+
+                    break;
+
+                case 7:
+
+                    break;
+
+                case 8:
+
+                    break;
+
+                case 9:
+
+                    break;
+
+                case 10:
+
+                    break;
+
+                case 11:
+
+                    break;
+
+                case 12:
+
+                    break;
+
+                case 13:
+
+                    break;
+
                 default:
                     break;
             }
